457822694388c3ac05b55a8753f7e79ad975362a
Martin Oywa <martinoywa2@gmail.com>
2020-04-17 01:48:37 +0300

    Add Home activity and its fragments

---
+++ app/src/main/java/com/lucosa/crowdnotes/HomeActivity.java
@@ -0,0 +1,63 @@
+package com.lucosa.crowdnotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.PagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private TabItem home, notification, profile, others;
+    public PageAdapter pageAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        tabLayout = findViewById(R.id.tab_layout);
+        home = findViewById(R.id.home_page);
+        notification = findViewById(R.id.notification_page);
+        profile = findViewById(R.id.profile_page);
+        others = findViewById(R.id.others_page);
+        viewPager = findViewById(R.id.view_pager);
+
+        pageAdapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pageAdapter);
+
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+
+                if (tab.getPosition() == 0) {
+                    pageAdapter.notifyDataSetChanged();
+                } else if (tab.getPosition() == 1) {
+                    pageAdapter.notifyDataSetChanged();
+                } else if (tab.getPosition() == 2) {
+                    pageAdapter.notifyDataSetChanged();
+                } else if (tab.getPosition() == 3) {
+                    pageAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+    }
+}
