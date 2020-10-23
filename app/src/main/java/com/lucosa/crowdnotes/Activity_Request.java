7c6e332da9d48ce52a734ee265c00c37e5c703e1
edwinnab <edwinnabikeri@gmail.com>
2020-10-23 11:13:26 +0300

    project

---
+++ app/src/main/java/com/lucosa/crowdnotes/Activity_Request.java
@@ -0,0 +1,14 @@
+package com.lucosa.crowdnotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Request extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__request);
+    }
+}
---
+++ app/src/main/java/com/lucosa/crowdnotes/Activity_Search.java
@@ -0,0 +1,14 @@
+package com.lucosa.crowdnotes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_Search extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__search);
+    }
+}
